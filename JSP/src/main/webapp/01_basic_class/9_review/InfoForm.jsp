715e54db0de2040fdf55dc2efa0f0881e861018f
Jeongwoo0o <dkthnt06@gmail.com>
2024-04-22 18:09:12 +0900

    commit

--- JSP/src/main/webapp/01_basic_class/9_review/InfoForm.jsp
+++ JSP/src/main/webapp/01_basic_class/9_review/InfoForm.jsp
@@ -18,28 +18,28 @@
 <table>
 	<tr>
 		<td>아이디</td>
-		<td><input name='User' type='text'  placeholder="5자~10자 영어와 숫자조합"></td>
+		<td><input name='User' type='text'  placeholder="5자~10자 영어와 숫자조합" pattern="[A-Za-z0-9]{5,10}" required></td>
 		<td><input type="button" value="중복확인"></td>
 	</tr>
 	<tr>
 		<td>비밀번호</td>
-		<td><input name='Pass' type='password'  placeholder="4자이상 영어와 숫자조합"></td>
+		<td><input name='Pass' type='password'  placeholder="4자이상 영어와 숫자조합" pattern="[A-Za-z0-9]{4,}" required></td>
 	</tr>
 	<tr>
 		<td>비밀번호확인</td>
-		<td><input name='Passcheck' type='password'></td>
+		<td><input name='Passcheck' type='password' required></td>
 	</tr>
 	<tr>
 		<td>이름</td>
-		<td><input name='Name' type='text' placeholder="한글 5자 이내"></td>
+		<td><input name='Name' type='text' placeholder="한글 5자 이내" pattern="[가-힣]{,5}" required></td>
 	</tr>
 	<tr>
 		<td>전화번호</td>
-		<td><input name='Tel' type='number'  placeholder="-없이 숫자조합"></td>
+		<td><input name='Tel' type='text'  placeholder="-없이 숫자조합" pattern="[0-9]{,11}" required></td>
 	</tr>
 	<tr>
 		<td>주소</td>
-		<td><input name='Addr' type='text' placeholder="20자 이내"></td>
+		<td><input name='Addr' type='text' placeholder="20자 이내" maxlength="20" required></td>
 	</tr>
 	<tr>
 		<td><input type='submit' value='회원가입'></td>
--- JSP/src/main/webapp/01_basic_class/9_review/js/info.js
+++ JSP/src/main/webapp/01_basic_class/9_review/js/info.js
@@ -1,40 +1,29 @@
 $(function() {
-	var user = $('input:eq(0)').val();
-    var pass = $('input:nth-child(2)').val();
-    var passCheck = $('input:nth-child(3)').val();
-    var name = $('input:nth-child(4)').val();
-    var tel = $('#input:nth-child(5)').val();
-    var addr = $('input:nth-child(6)').val();
-    alert(user);
-    if (user.length < 5 || user.length > 10) {
-        alert("아이디는 5자 이상, 10자 이하여야 합니다.");
-        return false;
-    }
-
-    if (pass.length < 4) {
-        alert("비밀번호는 4자 이상이어야 합니다.");
-        return false;
-    }
-
-    if (pass !== passCheck) {
-        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
-        return false;
-    }
-
-    if (name.length > 5) {
-        alert("이름은 5자 이내여야 합니다.");
-        return false;
-    }
-
-    if (tel.length !== 11) {
-        alert("전화번호는 11자리여야 합니다.");
-        return false;
-    }
-
-    if (addr.length > 20) {
-        alert("주소는 20자 이내여야 합니다.");
-        return false;
-    }
+	let st = 0;
+	$("input:eq(1)").click(function() {
+		let user 		= $('input[name="User"]').val();
+    	st++;
+	});
+	
+	$("input:eq(7)").click(function() {
+		let user 		= $('input[name="User"]').val();
+	    let pass 		= $('input[name="Pass"]').val();
+	    let passCheck 	= $('input[name="Passcheck"]').val();
+	    let name 		= $('input[name="Name"]').val();
+	    let tel 		= $('input[name="Tel"]').val();
+	    let addr 		= $('input[name="Addr"]').val();
+	    
+		if(st != 1) {
+			alert("아이디 중복확인을 눌러주세요.");
+			return false;
+		}
+		if(pass != passCheck){
+			alert("비밀번호가 일치하지 않습니다.");
+			return false;
+		}
+	});
+	
+    st = 0;
 
     return true;
 });
